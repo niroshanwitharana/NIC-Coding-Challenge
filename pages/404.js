edd6ebc4994d9f7ae2c932a8dff16dc021808896
niroshanwitharana <nrosh_uoc@yahoo.com>
2023-03-12 23:36:53 +0000

    time setup for redirect

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,19 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react'
 
 const NotFound = () => {
+    // this router object have a method call go, we can use it to redirect the page
+    const router = useRouter()
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            // router.go(-1) will take one stage to history 0r router.go(1) will take it forward
+            router.push('/') 
+
+        }, 3000)
+    }, [])
+
     return ( 
         <div className="not-found">
             <h1>Ooooops...</h1>
