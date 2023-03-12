8260639dbd7e956ed7318e71d6e5af326e3fa8b1
niroshanwitharana <nrosh_uoc@yahoo.com>
2023-03-12 22:29:00 +0000

    initial page setup

---
+++ pages/events/index.js
@@ -0,0 +1,11 @@
+const events = () => {
+    return ( 
+        <div>
+            <h2>Events List</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum voluptas inventore similique odio facere est, deleniti quam nulla voluptatem quae omnis, doloribus maxime. Sapiente, minima? Itaque, ea? Ab, iste atque!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum voluptas inventore similique odio facere est, deleniti quam nulla voluptatem quae omnis, doloribus maxime. Sapiente, minima? Itaque, ea? Ab, iste atque!</p>
+        </div>
+     );
+}
+ 
+export default events;
